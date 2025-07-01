37a80892f74015dc6be9040ab7cd3d7a9ad6addf
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-07-01 06:04:04 +0530

    <Refac:Windowrule>

--- scripts/shuffle_wallpaper.sh
+++ scripts/shuffle_wallpaper.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
 COUNTDOWN=60
+
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+CONFIG_DIR="$XDG_CONFIG_HOME/ags"
+
 while true; do
   sleep "$COUNTDOWN"
+
   IMAGE=$(find "$WALLPAPER_DIR" -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" \) | shuf -n1)
-  [[ -n "$IMAGE" ]] && swww img "$IMAGE" --transition-fps 60 --transition-step 100 --transition-type grow
+
+  if [[ -n "$IMAGE" ]]; then
+    swww img "$IMAGE" \
+      --transition-fps 60 \
+      --transition-step 100 \
+      --transition-type grow
+
+    # Generate colors for ags
+    "$CONFIG_DIR"/scripts/color_generation/colorgen.sh "$IMAGE" --apply --smart
+  fi
 done
